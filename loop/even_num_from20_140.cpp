c227e0de54190f124f5a046391441bb50aa207a5
piyushdwiedi-devloper <piyushdwivedi@gmail.com>
2026-01-23 23:01:39 +0530

    new commit

---
+++ loop/even_num_from20_140.cpp
@@ -0,0 +1,33 @@
+  #include <iostream>
+using namespace std; 
+
+int main(){
+for(int i=20; i<=140;i +=2){
+ cout <<i<<"   ";   
+}
+
+return 0;
+
+}      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
