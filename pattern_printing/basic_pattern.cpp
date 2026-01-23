d8caf5baf1194917d6e6ab1c99938517bdf948e1
piyushdwiedi-devloper <piyushdwivedi@gmail.com>
2026-01-23 22:44:54 +0530

    new commit

---
+++ pattern_printing/basic_pattern.cpp
@@ -0,0 +1,32 @@
+  #include <iostream>
+using namespace std; 
+
+int main(){
+int m;
+cout <<"enter number of rows";
+cin>>m;
+for(int i=1; i<=m;i++){
+cout<<"*****"<<endl;
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
